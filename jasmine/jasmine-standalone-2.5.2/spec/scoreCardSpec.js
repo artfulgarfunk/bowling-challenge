3024f05816d158cc41ef0b940bd4cd4713b241ca
Jack <92jackhenderson@gmail.com>
2017-02-05 13:53:38 +0000

    spare bonus functionality added

--- jasmine/jasmine-standalone-2.5.2/spec/scoreCardSpec.js
+++ jasmine/jasmine-standalone-2.5.2/spec/scoreCardSpec.js
@@ -12,14 +12,47 @@ describe('scoreCard', function(){
     });
 
     it('have an array of the number of pins knocked down', function(){
-      expect(scorecard.pins).toEqual([])
+      expect(scorecard.frames).toEqual([])
     });
 
-    it('record the number of pins knocked down in each bowl', function(){
-      scorecard.bowl(5)
-      scorecard.bowl(2)
-      expect(scorecard.pins[0]).toEqual(5)
-      expect(scorecard.pins[1]).toEqual(2)
-    })
+    // it('record the number of pins knocked down in each bowl', function(){
+    //   scorecard.bowl(5)
+    //   scorecard.bowl(2)
+    //   expect(scorecard.pins[0]).toEqual(5)
+    //   expect(scorecard.pins[1]).toEqual(2)
+    // });
+
+    it('add the number of pins knocked down to the players total score', function(){
+      scorecard.bowl(5,2)
+      expect(scorecard.score).toEqual(7)
+    });
+
+    it('record the number of pins knocked down as pair of scores in a new frame array', function() {
+      scorecard.bowl(5,2)
+      expect(scorecard.frames[0][0]).toEqual(5)
+      expect(scorecard.frames[0][1]).toEqual(2)
+    });
+
+    it('activate the Spare Bonus when the player get a spare', function(){
+      scorecard.bowl(5,5)
+      expect(scorecard.spareBonus).toBe(true)
+    });
+
+    it('deactivate the Spare Bonus when the player does not get a spare', function(){
+      scorecard.bowl(4,5)
+      expect(scorecard.spareBonus).toBe(false)
+    });
+
+    it('add double the score of the next bowl (first bowl of the next frame) after a spare', function() {
+      scorecard.bowl(5,5)
+      expect(scorecard.spareBonus).toBe(true)
+      expect(scorecard.score).toBe(10)
+      scorecard.bowl(4,5)
+      expect(scorecard.score).toEqual(23);
+    });
+
+    // it('add double the score of the next 2 bowls when the player gets a strike', function {
+    //
+    // })
   });
 })
--- jasmine/jasmine-standalone-2.5.2/src/scoreCard.js
+++ jasmine/jasmine-standalone-2.5.2/src/scoreCard.js
@@ -2,9 +2,30 @@
 
 function scoreCard () {
   this.score = 0;
-  this.pins = []
+  this.frames = []
+  this.spareBonus = false
 };
 
-scoreCard.prototype.bowl = function(pins) {
-  (this.pins).push(pins);
+scoreCard.prototype.bowl = function(pins1, pins2) {
+    var frame = [];
+    frame.push(pins1);
+    frame.push(pins2);
+    this.frames.push(frame);
+    if (this.spareBonus === false)
+    { this.score += pins1;
+    this.score += pins2;
+    this.spareBonusCheck(pins1,pins2);
+  } else {
+    this.score += (pins1 * 2);
+    this.score += pins2;
+    this.spareBonusCheck(pins1,pins2);
+  };
 };
+
+scoreCard.prototype.spareBonusCheck = function(pins1, pins2) {
+  if (pins1 + pins2 >= 10)
+  {this.spareBonus = true}
+  else {
+    this.spareBonus = false
+  };
+}
